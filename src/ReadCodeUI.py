4010aee3b183c0835064e95800c5aeb18c7191f0
yifan <yfyang.86@gmail.com>
2015-04-08 10:17:06 -0400

    scm example

--- src/ReadCodeUI.py
+++ src/ReadCodeUI.py
@@ -163,6 +163,6 @@ if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)
     Dialog = QtGui.QDialog()
     ui = Ui_Dialog()
-    ui.setupUi(Dialog, "D:/code")
+    ui.setupUi(Dialog, "/homr/yifan")
     Dialog.show()
     sys.exit(app.exec_())
